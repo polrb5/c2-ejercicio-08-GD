e62060247635963a6924525640414530a850151f
“polrb5” <polreigbroto@gmail.com>
2021-06-02 17:14:39 +0200

    Creadas clases de luchador, asesor y escudero

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable lines-between-class-members */
+/* eslint-disable max-classes-per-file */
+
 class Personaje {
   tipo;
   serie;
@@ -5,6 +8,7 @@ class Personaje {
   edad;
   estado;
   familia;
+
   constructor(nombre, anyos, serie, tipo, estado, familia) {
     this.nombre = nombre;
     this.edad = anyos;
@@ -24,11 +28,67 @@ class Personaje {
 class Rey extends Personaje {
   anyosReinado;
   mensaje;
+
   constructor(nombre, anyos, serie, tipo, estado, familia, anyosReinado) {
     super(nombre, anyos, serie, tipo, estado, familia);
     this.anyosReinado = anyosReinado;
     this.mensaje = "Vais a morir todos";
-    super.comunicar(mensaje);
+    /*  super.comunicar(mensaje); */
+  }
+}
+class luchador extends Personaje {
+  armaQueUsa;
+  destreza;
+  mensaje;
+
+  constructor(
+    nombre,
+    anyos,
+    serie,
+    tipo,
+    estado,
+    familia,
+    armaQueUsa,
+    destreza
+  ) {
+    super(nombre, anyos, serie, tipo, estado, familia);
+    this.armaQueUsa = armaQueUsa;
+    this.destreza = destreza;
+    this.mensaje = "Primero pego y luego pregunto";
+    /*  super.comunicar(mensaje); */
+  }
+}
+class asesor extends Personaje {
+  personajeAsesorado;
+  mensaje;
+
+  constructor(nombre, anyos, serie, tipo, estado, familia, personajeAsesorado) {
+    super(nombre, anyos, serie, tipo, estado, familia);
+    this.personajeAsesorado = personajeAsesorado;
+    this.mensaje = "No sé por qué, pero creo que voy a morir pronto";
+    /*  super.comunicar(mensaje); */
+  }
+}
+class escudero extends Personaje {
+  personajeServido;
+  gradoPelotismo;
+  mensaje;
+
+  constructor(
+    nombre,
+    anyos,
+    serie,
+    tipo,
+    estado,
+    familia,
+    personajeServido,
+    gradoPelotismo
+  ) {
+    super(nombre, anyos, serie, tipo, estado, familia);
+    this.personajeServido = personajeServido;
+    this.gradoPelotismo = gradoPelotismo;
+    this.mensaje = "Soy un loser";
+    /*  super.comunicar(mensaje); */
   }
 }
 
@@ -41,4 +101,5 @@ const joffrey = new Rey(
   "Lannister",
   700
 );
-console.log(joffrey.comunicar(mensaje));
+console.log(joffrey);
+console.log(joffrey.comunicar("hola"));
